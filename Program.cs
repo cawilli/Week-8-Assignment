9083a2d70ee90ce70b3c8c7f1971f60c1e15c27f
Candace Williams <candace.williams94@hotmail.com>
2021-09-05 08:48:00 -0400

    add gitignore file and project folders.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week_8_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
