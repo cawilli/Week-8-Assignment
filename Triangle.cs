5077ee1a841596dbba582775347b32fa0433ac33
Candace Williams <candace.williams94@hotmail.com>
2021-09-05 13:11:02 -0400

    add triangle class.

---
+++ Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_8_Assignment
+{
+    class Triangle : Shapes
+    {
+        public int sideA;
+        public int sideB;
+        public int sideC;
+
+        public void TriangleSides()
+        {
+            Console.WriteLine($"Printed List: {ShapeType} | {Color}| {sideA} |{sideB} | {sideC}");
+        }
+    }
+}
