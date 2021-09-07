5bb61a48624d19109aa13a35d07f0d56f36e2664
Candace Williams <candace.williams94@hotmail.com>
2021-09-07 16:28:36 -0400

    uncomment out line of code to work.

--- Triangle.cs
+++ Triangle.cs
@@ -12,14 +12,14 @@ namespace Week_8_Assignment
 
         public void TriangleSides()
         {
-            //Console.WriteLine("Input Side A.");
-            //int SideA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Side A.");
+            int SideA = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Input Side B");
-            //int SideB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Side B");
+            int SideB = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine("Input Side C.");
-            //int SideC = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Side C.");
+            int SideC = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Printed List: {ShapeType} | {Color}| {SideA} |{SideB} | {SideC}");
         }
